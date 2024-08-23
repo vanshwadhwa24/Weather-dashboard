d46d27fc90904999aaf8eaabb1e6e996923c3517
vanshwadhwa24 <vanshreads@gmail.com>
2024-08-23 12:57:57 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const CONFIG = {
+    API_KEY: "96509125e21d68ea12ba5e183e3cebd3", // replace YOUR_NEW_API_KEY with your new OpenWeatherMap API key
+  };
+  
+  export default CONFIG;
+  
